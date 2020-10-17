fc0b56f8d2d440a1aeac390b081de73f4a7ddd98
snowman-art <61948720+snowman-art@users.noreply.github.com>
2020-10-17 14:23:53 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const { CommandoClient } = require('@iceprod/discord.js-commando');
+const path = require('path');
+
+const client = new CommandoClient({
+	commandPrefix: '++',
+	owner: '358042934748381194',
+	invite: 'https://discord.gg/2yERsg2',
+});
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['information', 'Your First Command Group'],
+		['ordering', 'Your Second Command Group'],
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands({
+	           unknownCommand: false,
+	    	help: false,
+	})
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+	
+	client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity(`in the kitchen for ${client.guilds.cache.size} servers! `);
+});
+
+client.on('error', console.error);
+
+client.login('NzIyNjM2ODM4MTkxMjM1MTAz.Xul-AA.GDzAR5mHJNaLBlQNaMh9pQsFS9c');
